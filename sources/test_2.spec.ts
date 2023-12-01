e86c656e5a9bec69579e06e4620a8f31c9d547b5
Howard Peng <howard.pen9@gmail.com>
2023-12-01 19:11:24 +0700

    uu

--- sources/test_2.spec.ts
+++ sources/test_2.spec.ts
@@ -1,4 +1,4 @@
-import { toNano, beginCell } from "ton";
+import { toNano, beginCell, Address, Cell } from "ton";
 import {
     Blockchain,
     SandboxContract,
@@ -8,60 +8,43 @@ import {
 } from "@ton-community/sandbox";
 import "@ton-community/test-utils";
 
-import { NftCollection } from "./output/sample_NftCollection";
+import { AA } from "./output/sample_AA";
 import { NftItem } from "./output/sample_NftItem";
 
-describe("contract", () => {
-    const OFFCHAIN_CONTENT_PREFIX = 0x01;
-    const string_first = "https://s.getgems.io/nft-staging/c/628f6ab8077060a7a8d52d63/";
-    let newContent = beginCell().storeInt(OFFCHAIN_CONTENT_PREFIX, 8).storeStringRefTail(string_first).endCell();
+let JettonMinterAddress = Address.parse("EQAiQ2XK7BXePLwemeo-u4wNyjg-wxGeySmaFGEP7R2MhUWs");
 
+// Jetton Master, get Jetton Wallet Address:
+// https://testnet.ton.cx/address/kQAiQ2XK7BXePLwemeo-u4wNyjg-wxGeySmaFGEP7R2Mhf4m
+
+// const dataCell = Cell.fromBase64("te6cckEBAQEAIwBAQgJQQJUQZQAYuEnu4sHWweyGQ0OM6hxbG3vdtQwe2G+DKq6KcYs=");
+let hexString = "b5ee9c720101010100230000420250409510650018b849eee2c1d6c1ec8643438cea1c5b1b7bddb50c1ed86f832a";
+let buffer = Buffer.from(hexString, "hex").toString("base64"); // hexString to base64
+let dataCell = Cell.fromBase64(buffer);
+
+describe("contract", () => {
     let blockchain: Blockchain;
     let deployer: SandboxContract<TreasuryContract>;
-    let collection: SandboxContract<NftCollection>;
+    let contract: SandboxContract<AA>;
 
     beforeAll(async () => {
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury("deployer");
 
-        collection = blockchain.openContract(
-            await NftCollection.fromInit(deployer.address, newContent, {
-                $$type: "RoyaltyParams",
-                numerator: 350n, // 350n = 35%
-                denominator: 1000n,
-                destination: deployer.address,
-            })
-        );
-
-        const deploy_result = await collection.send(deployer.getSender(), { value: toNano(1) }, "Mint");
+        contract = blockchain.openContract(await AA.fromInit(dataCell, JettonMinterAddress));
+        const deploy_result = await contract.send(deployer.getSender(), { value: toNano(1) }, "C");
         expect(deploy_result.transactions).toHaveTransaction({
             from: deployer.address,
-            to: collection.address,
+            to: contract.address,
             deploy: true,
             success: true,
         });
     });
 
     it("Test", async () => {
-        console.log("Next IndexID: " + (await collection.getGetCollectionData()).next_item_index);
-        console.log("Collection Address: " + collection.address);
-    });
+        let aa = await contract.getGetWalletAddress(Address.parse("kQAgzVlCkPrK9r8F3J1Dgxf8OGwY46yTynBWrU_s4WaJRPtQ"));
+        console.log(aa.toString());
 
-    it("should deploy correctly", async () => {
-        let current_index = (await collection.getGetCollectionData()).next_item_index;
-
-        const deploy_result = await collection.send(deployer.getSender(), { value: toNano(13) }, "Mint"); // Send Mint Transaction
-        expect(deploy_result.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: collection.address,
-            success: true,
-        });
-
-        let next_index = (await collection.getGetCollectionData()).next_item_index;
-        expect(next_index).toEqual(current_index + 1n);
-        // console.log("Next IndexID: " +);
-
-        // console.log(printTransactionFees(deploy_result.transactions));
-        // console.log(prettyLogTransactions(deploy_result.transactions));
+        // let walletCode = await contract.getGetWalletCode();
+        // console.log(walletCode.asBuilder());
     });
 });
